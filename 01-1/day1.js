4fc4fce85865b72a9417eaa488128324aad356ff
Otto <justigo86@gmail.com>
2022-12-01 16:26:54 -0500

    finished day one, part two

--- 01-1/day1.js
+++ 01-1/day1.js
@@ -7,11 +7,12 @@ const fs = require('fs');
 //   .map((item) => parseInt(item));   //make each line of data a number
 
 const arr = fs
-  .readFileSync("data.txt", { encoding: "utf-8" })
-  .split('\r\n\r\n')
-  .map((item) => item.split('\r\n'))
-  .map((item) => item.map(x => parseInt(x)))
-  .map((item) => item.reduce((a, b) => a + b, 0))
-  .sort((a, b) => b - a)
+  .readFileSync("data.txt", { encoding: "utf-8" })  //read the txt file and return the content as string
+  .split('\r\n\r\n')    //split string on each empty line into individual arrays
+  .map((item) => item.split('\r\n'))    //split each array string into separate strings
+  .map((item) => item.map(x => parseInt(x)))    //parse each string item into a number
+  .map((item) => item.reduce((a, b) => a + b, 0))   //add up numbers within each array
+  .sort((a, b) => b - a)    //sort array from highest to lowest number
 
-console.log(arr[0])
+// console.log(arr[0])
+console.log(arr[0] + arr[1] + arr[2])
